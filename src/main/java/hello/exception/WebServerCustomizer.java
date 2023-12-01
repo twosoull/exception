0f602a3c4dec5b905ca9d72ffdd2918632744717
iyeonghun <twosoull@naver.com>
2023-12-01 20:42:18 +0900

    예외 발생시 에러페이지로 리다이렉트 될 경우에 filterRegistrationBean에서 dispatcherType으로 조절할 수 있다.

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        //런타임 또는 그 자식 타입의 에러일 경우 500 (500 예외가 서버내부에서 발생한 오류라는 뜻을 포함하고 있기 때문에 여기서는 예외가
+        //발생한 경우도 500 오류 화면으로 처리했다.
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
+
+    }
+}
